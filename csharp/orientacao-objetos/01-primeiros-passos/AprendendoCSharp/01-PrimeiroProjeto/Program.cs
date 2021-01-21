f67fbf2964cc03675eb5db08743046cfda45f0ab
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-21 20:01:17 -0300

    Primeiros passos: Adicionando PrimeiroProjeto

---
+++ csharp/orientacao-objetos/01-primeiros-passos/AprendendoCSharp/01-PrimeiroProjeto/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01_PrimeiroProjeto
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Olá, mundo. Projeto no VS Code!");
+
+      Console.WriteLine("A execução acabou. Tecle enter para finalizar...");
+      Console.ReadLine();
+    }
+  }
+}
