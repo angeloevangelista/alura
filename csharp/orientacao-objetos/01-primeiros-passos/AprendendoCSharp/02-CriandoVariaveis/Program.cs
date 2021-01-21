1582195546233450afb0d3c32755145e23bba594
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-21 20:15:50 -0300

    Primeiros passos: Usando variaveis Int

---
+++ csharp/orientacao-objetos/01-primeiros-passos/AprendendoCSharp/02-CriandoVariaveis/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _02_CriandoVariaveis
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Executando projeto 2 - Criando Variáveis.");
+
+      int idade;
+      idade = 32;
+
+      Console.WriteLine(idade);
+
+      idade = 10;
+      Console.WriteLine(idade);
+
+      idade = 10 + 5;
+      Console.WriteLine(idade);
+
+      idade = 10 + 5 * 2;
+      Console.WriteLine(idade);
+
+      idade = (10 + 5) * 2;
+      Console.WriteLine(idade);
+      Console.WriteLine("A sua idade é" + idade + ", parabéns!");
+
+      Console.WriteLine("A execução acabou. Tecle enter para finalizar...");
+      Console.ReadLine();
+    }
+  }
+}
