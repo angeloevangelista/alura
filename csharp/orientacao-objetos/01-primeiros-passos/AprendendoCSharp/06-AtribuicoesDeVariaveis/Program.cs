0a5db5db1c42cb0a9f6b9dbd69b8fdd3220cd14a
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-21 22:02:58 -0300

    Primeiros passos: Atribuicoes de variaveis

---
+++ csharp/orientacao-objetos/01-primeiros-passos/AprendendoCSharp/06-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _06_AtribuicoesDeVariaveis
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      Console.WriteLine("Executando projeto 6.");
+
+      int idade = 32;
+      int idadeGustavo = idade;
+
+      idade = 20;
+
+      Console.WriteLine(idade);
+      Console.WriteLine(idadeGustavo);
+
+      Console.ReadLine();
+    }
+  }
+}
