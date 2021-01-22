8d83de8b247c5e3baf215a2300a4bbde139be3d3
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-21 21:28:55 -0300

    Primeiros passos: Conversoes e tipos numericos

---
+++ csharp/orientacao-objetos/01-primeiros-passos/AprendendoCSharp/04-ConversoesEOutrosTipoNumericos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _03_CriandoVariaveisPontoFlutuante
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(
+        "Executando projeto 4 - Conversões."
+      );
+
+      double salario = 1200.50;
+
+      // O long tem 32 bits
+      int salarioEmInteiro = (int)salario;
+      Console.WriteLine(salarioEmInteiro);
+
+
+      // O long tem 64 bits
+      long idade = 13000000000;
+      Console.WriteLine(idade);
+
+      // O long tem 16 bits
+      short quantidadeProdutos = 15000;
+      Console.WriteLine(quantidadeProdutos);
+
+      float altura = 1.65f;
+      Console.WriteLine(altura);
+
+      Console.WriteLine("A execução acabou. Tecle enter para finalizar...");
+      Console.ReadLine();
+    }
+  }
+}
