ef9562511e27494c4a33c76c3bab5372bf9d8730
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-22 09:31:19 -0300

    Primeiros passos: Escopo

---
+++ csharp/orientacao-objetos/01-primeiros-passos/AprendendoCSharp/09-Escopo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _09_Escopo
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Executando projeto 9 - Escopo.");
+
+      int idadeJoao = 18;
+      bool acompanhado = false;
+      string mensagemAdicional;
+
+      if (acompanhado)
+        mensagemAdicional = "João está acompanhado.";
+      else
+      {
+        mensagemAdicional = "João não está acompanhado.";
+        Console.WriteLine(mensagemAdicional);
+      }
+
+      if (idadeJoao >= 18 || acompanhado)
+      {
+        Console.WriteLine("Pode entrar.");
+        Console.WriteLine(mensagemAdicional);
+      }
+      else
+      {
+        Console.WriteLine("Não pode entrar.");
+      }
+
+      Console.ReadLine();
+    }
+  }
+}
