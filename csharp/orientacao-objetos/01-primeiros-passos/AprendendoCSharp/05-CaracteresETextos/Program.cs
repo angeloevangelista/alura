7b35baa70cb62c25c877cb5fbcf23992ff2f86cb
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-21 21:46:30 -0300

    Primeiros passos: Caracteres e textos

---
+++ csharp/orientacao-objetos/01-primeiros-passos/AprendendoCSharp/05-CaracteresETextos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _05_CaracteresETextos
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(
+        "Executando projeto 5 - Caracteres e textos."
+      );
+
+      char primeiraLetra = 'a';
+      Console.WriteLine(primeiraLetra);
+
+      primeiraLetra = (char)65;
+      Console.WriteLine(primeiraLetra);
+
+      primeiraLetra = (char)(primeiraLetra + 1);
+      Console.WriteLine(primeiraLetra);
+
+      string titulo = "Alura Cursos de tecnologia " + 2020;
+      string cursosProgramacao = @"- .NET
+- Java
+- JavaScript";
+
+      Console.WriteLine(titulo);
+      Console.WriteLine(cursosProgramacao);
+
+      Console.ReadLine();
+    }
+  }
+}
