361f336c4e8e49d3d98e73f395351ae1243d64a3
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-22 13:39:09 -0300

    Primeiros passos: Repetiçao com while

---
+++ csharp/orientacao-objetos/01-primeiros-passos/AprendendoCSharp/10-CalculandoPoupanca/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _10_CalculandoPoupanca
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Executando projeto 10 - Calculando poupança.");
+
+      double valorInvestido = 1000;
+
+      int contadorMes = 1;
+
+      while (contadorMes <= 12)
+      {
+        valorInvestido = valorInvestido + valorInvestido * 0.0036;
+        Console.WriteLine("Após " + contadorMes + " meses, você terá R$" + valorInvestido);
+        contadorMes++;
+      }
+
+      Console.ReadLine();
+    }
+  }
+}
