626ba71a868c043a8e5fb3ce386a78f039c41271
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-22 16:22:24 -0300

    Primeiros passos: Repetiçao com for

---
+++ csharp/orientacao-objetos/01-primeiros-passos/AprendendoCSharp/11-CalculandoPoupanca2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _11_CalculandoPoupanca2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Executando projeto 11 - Calculando poupança 2.");
+
+      double valorInvestido = 1000;
+
+      for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+      {
+        valorInvestido *= 1.0036;
+
+        Console.WriteLine(
+          "Após " + contadorMes + " meses, você terá R$" + valorInvestido
+        );
+      }
+
+      Console.ReadLine();
+    }
+  }
+}
