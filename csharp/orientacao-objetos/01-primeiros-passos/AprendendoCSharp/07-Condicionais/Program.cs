51efa9141f199fbbf0eddeb2169cc9f68ade3172
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-21 22:56:56 -0300

    Primeiros passos: Condicionais

---
+++ csharp/orientacao-objetos/01-primeiros-passos/AprendendoCSharp/07-Condicionais/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _07_Condicionais
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Executando projeto 7 - Condicionais.");
+
+      int idadeJoao = 18;
+      int quantidadePessoas = 2;
+
+      if (idadeJoao >= 18)
+      {
+        Console.WriteLine("João possui mais de 18 anos idade, pode entrar.");
+      }
+      else
+      {
+        if (quantidadePessoas > 1)
+        {
+          Console.WriteLine(
+            "João não possui mais de 18 anos, mas está acompanhado. Pode entrar."
+          );
+        }
+        else
+        {
+          Console.WriteLine("João não possui mais de 18 anos, não pode entrar.");
+        }
+      }
+
+      Console.ReadLine();
+    }
+  }
+}
