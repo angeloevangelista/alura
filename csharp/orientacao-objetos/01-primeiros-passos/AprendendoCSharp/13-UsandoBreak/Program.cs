61811be2694100f590d999d36237c4f7c2ccca89
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-22 21:39:38 -0300

    Primeiros passos: Usando break

---
+++ csharp/orientacao-objetos/01-primeiros-passos/AprendendoCSharp/13-UsandoBreak/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _13_UsandoBreak
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(
+        "Executando projeto 13 - Usando o break."
+      );
+
+      // Usando o BREAK
+      for (int contadorLinha = 1; contadorLinha <= 10; contadorLinha++)
+      {
+        for (int contadorColuna = 1; contadorColuna <= 10; contadorColuna++)
+        {
+          Console.Write('*');
+
+          if (contadorColuna == contadorLinha)
+            break;
+        }
+
+        Console.Write("\n");
+      }
+
+      // "Give me another way!"
+      for (int contadorLinha = 1; contadorLinha <= 10; contadorLinha++)
+      {
+        for (int contadorColuna = 1; contadorColuna <= contadorLinha; contadorColuna++)
+          Console.Write('*');
+
+        Console.Write("\n");
+      }
+
+      Console.ReadLine();
+    }
+  }
+}
