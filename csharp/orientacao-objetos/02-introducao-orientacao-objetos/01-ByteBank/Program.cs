f4c6c3b15859d234682b9d280251494f345f0f96
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-22 23:53:47 -0300

    Introduçao POO: Instanciando a Gaby

--- csharp/orientacao-objetos/02-introducao-orientacao-objetos/01-ByteBank/Program.cs
+++ csharp/orientacao-objetos/02-introducao-orientacao-objetos/01-ByteBank/Program.cs
@@ -6,7 +6,17 @@ namespace _01_ByteBank
   {
     static void Main(string[] args)
     {
-      new ContaCorrente();
+      ContaCorrente contaCorrenteGabriela = new ContaCorrente();
+
+      contaCorrenteGabriela.titular = "Gabriela";
+      contaCorrenteGabriela.agencia = 863;
+      contaCorrenteGabriela.numero = 863452;
+      contaCorrenteGabriela.saldo = 9001;
+
+      Console.WriteLine("Titular: " + contaCorrenteGabriela.titular);
+      Console.WriteLine("Agência: " + contaCorrenteGabriela.agencia);
+      Console.WriteLine("Número: " + contaCorrenteGabriela.numero);
+      Console.WriteLine("Saldo: " + contaCorrenteGabriela.saldo);
 
       Console.ReadLine();
     }
