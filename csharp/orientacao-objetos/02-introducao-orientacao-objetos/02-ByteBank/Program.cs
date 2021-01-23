6fd43d34938404216403adedd039517f4799df59
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-22 23:58:51 -0300

    Introduçao POO: Valores padrao

---
+++ csharp/orientacao-objetos/02-introducao-orientacao-objetos/02-ByteBank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02_ByteBank
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      ContaCorrente conta = new ContaCorrente();
+
+      conta.titular = "Gabriela";
+
+      Console.WriteLine("Titular: " + conta.titular);
+      Console.WriteLine("Agência: " + conta.agencia);
+      Console.WriteLine("Número: " + conta.numero);
+      Console.WriteLine("Saldo: " + conta.saldo);
+
+      Console.ReadLine();
+    }
+  }
+}
