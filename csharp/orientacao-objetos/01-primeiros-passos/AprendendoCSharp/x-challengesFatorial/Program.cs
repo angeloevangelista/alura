15f051a3d7af475c97104669c77e5385ed7e5419
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-22 22:00:09 -0300

    Primeiros passos: Accepting the challenge

---
+++ csharp/orientacao-objetos/01-primeiros-passos/AprendendoCSharp/x-challengesFatorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace x_challengesFatorial
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Desafios - Fatorial.");
+
+      int numero = 6;
+      int fatorial = 1;
+
+      for (int contador = 1; contador <= numero; contador++)
+        fatorial *= contador;
+
+      Console.WriteLine("O fatorial de " + numero + " é " + fatorial);
+
+      Console.ReadLine();
+    }
+  }
+}
