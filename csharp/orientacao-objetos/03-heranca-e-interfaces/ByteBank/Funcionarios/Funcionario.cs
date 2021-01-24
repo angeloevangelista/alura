eced29cc6d8f9c7c161a3008b770074f46a6db11
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-23 22:24:06 -0300

    Herança e Interfaces: Classe Funcionario

---
+++ csharp/orientacao-objetos/03-heranca-e-interfaces/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,14 @@
+namespace ByteBank.Funcionarios
+{
+  public class Funcionario
+  {
+    public string Nome { get; set; }
+    public string CPF { get; set; }
+    public double Salario { get; set; }
+
+    public double GetBonificacao()
+    {
+      return this.Salario * 0.1;
+    }
+  }
+}
---
+++ csharp/orientacao-objetos/03-heranca-e-interfaces/ByteBank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ByteBank.Funcionarios;
+
+namespace ByteBank
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Funcionario carlos = new Funcionario();
+      carlos.Nome = "Carlos";
+      carlos.CPF = "235.567.487-28";
+      carlos.Salario = 2355;
+
+      Console.WriteLine(carlos.Nome);
+      Console.WriteLine(carlos.GetBonificacao());
+      Console.ReadLine();
+    }
+  }
+}
