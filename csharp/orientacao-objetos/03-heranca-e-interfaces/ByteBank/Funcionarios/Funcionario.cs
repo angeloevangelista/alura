28acaf9f6d80a07c787996173d0a984ada4e7e42
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-23 23:07:18 -0300

    Herança e Interfaces: Herdando e sobrecrevendo

--- csharp/orientacao-objetos/03-heranca-e-interfaces/ByteBank/Funcionarios/Funcionario.cs
+++ csharp/orientacao-objetos/03-heranca-e-interfaces/ByteBank/Funcionarios/Funcionario.cs
@@ -6,7 +6,7 @@ namespace ByteBank.Funcionarios
     public string CPF { get; set; }
     public double Salario { get; set; }
 
-    public double GetBonificacao()
+    public virtual double GetBonificacao()
     {
       return this.Salario * 0.1;
     }
--- csharp/orientacao-objetos/03-heranca-e-interfaces/ByteBank/Program.cs
+++ csharp/orientacao-objetos/03-heranca-e-interfaces/ByteBank/Program.cs
@@ -13,9 +13,18 @@ namespace ByteBank
       Funcionario carlos = new Funcionario();
       carlos.Nome = "Carlos";
       carlos.CPF = "235.567.487-28";
-      carlos.Salario = 2355;
+      carlos.Salario = 2000;
 
+      Console.WriteLine(carlos.Nome);
       gerenciadorBonificacao.Registrar(carlos);
+
+      Diretor roberta = new Diretor();
+      roberta.Nome = "Roberta";
+      roberta.CPF = "353.325.389-12";
+      roberta.Salario = 5000;
+
+      Console.WriteLine(roberta.Nome);
+      gerenciadorBonificacao.Registrar(roberta);
 
       Console.WriteLine(
         "Total de bonificações: " + gerenciadorBonificacao.GetTotalBonificacao()
