5c90c0d73945a69c94f844ae393e0d32d5db06eb
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-25 16:03:24 -0300

    Angular: Injetando HttpClient

--- angular/01-fundamentos/web/src/app/app.component.ts
+++ angular/01-fundamentos/web/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 interface IPhoto {
@@ -12,15 +13,19 @@ interface IPhoto {
 })
 export class AppComponent {
   photos: IPhoto[] = [
-    {
-      description: 'Leão',
-      url:
-        'https://images.unsplash.com/photo-1517649281203-dad836b4abe5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-    },
-    {
-      description: 'Baleia',
-      url:
-        'https://images.unsplash.com/photo-1512419944406-9e8e4d4fee8a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80',
-    },
+    // {
+    //   description: 'Leão',
+    //   url:
+    //     'https://images.unsplash.com/photo-1517649281203-dad836b4abe5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+    // },
+    // {
+    //   description: 'Baleia',
+    //   url:
+    //     'https://images.unsplash.com/photo-1512419944406-9e8e4d4fee8a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80',
+    // },
   ];
+
+  constructor(private httpClient: HttpClient) {
+    console.log(this.httpClient);
+  }
 }
