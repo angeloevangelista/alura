8f8012cf86fa209f21dea0360ba0f0f2bfeef874
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-25 13:08:29 -0300

    Angular: Data binding one way

--- angular/01-fundamentos/src/app/app.component.ts
+++ angular/01-fundamentos/src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'alura-pic';
+  description = 'Leão';
+  url =
+    'https://images.unsplash.com/photo-1517649281203-dad836b4abe5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80';
 }
