b6cb480cdd1c8a2ad56fd2c863a4b6e8b89e913e
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-25 00:29:35 -0300

    List, Lambda & Link: Conhecendo Enumerable

--- csharp/orientacao-objetos/08-list-lambda-linq/ByteBank.SistemaAgencia/Program.cs
+++ csharp/orientacao-objetos/08-list-lambda-linq/ByteBank.SistemaAgencia/Program.cs
@@ -26,9 +26,9 @@ namespace ByteBank.SistemaAgencia
 
       // contas.Sort(new ComparadorContaCorrentePorAgencia());
 
-      var contasNaoNulas = contas.Where(conta => conta != null);
-
-      var contasOrdenadas = contasNaoNulas.OrderBy(conta => conta.Numero);
+      var contasOrdenadas = contas
+        .Where(conta => conta != null)
+        .OrderBy(conta => conta.Numero);
 
       foreach (var conta in contasOrdenadas)
       {
