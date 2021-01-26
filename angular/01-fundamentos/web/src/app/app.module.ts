dbdf1d708a03ad28ad365eba4c182bb40a4bd97d
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-26 15:01:20 -0300

    Angular:  Usando AppRoutingModule

--- angular/01-fundamentos/web/src/app/app.module.ts
+++ angular/01-fundamentos/web/src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PhotosModule],
+  imports: [BrowserModule, AppRoutingModule, PhotosModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ angular/01-fundamentos/web/src/app/photos/photo-form/photo-form.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ap-photo-form',
+  templateUrl: './photo-form.component.html',
+})
+export class PhotoFormComponent {}
