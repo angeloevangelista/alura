aebf5c0f3cf4a69ceb8ceb1095fe66d932529a4c
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-01-26 23:50:38 -0300

    Angular:  Usando NotFoundComponent

--- angular/01-fundamentos/web/src/app/app.module.ts
+++ angular/01-fundamentos/web/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { AppRoutingModule } from './app.routing.module';
+import { ErrorsModule } from './errors/errors.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PhotosModule],
+  imports: [BrowserModule, AppRoutingModule, PhotosModule, ErrorsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
