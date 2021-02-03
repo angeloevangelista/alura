7e0ee53bbf0da56b27a8bfc87d2aafde77bf7071
Angelo Evangelista <angeloevan.ane@gmail.com>
2021-02-02 21:06:49 -0300

    Angular: Usando ElementRef

--- angular/01-fundamentos/web/src/app/photos/photo-list/photo-list.module.ts
+++ angular/01-fundamentos/web/src/app/photos/photo-list/photo-list.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PhotoModule } from '../photo/photo.module';
+import { CardModule } from 'src/app/shared/components/card/card.module';
+import { DarkenOnHoverModule } from 'src/app/shared/directives/darken-on-hover/darken-on-hover.module';
+
 import { PhotoListComponent } from './photo-list.component';
 import { PhotosComponent } from './photos/photos.component';
 import { LoadButtonComponent } from './load-button/load-button.component';
+import { SearchComponent } from './search/search.component';
 
 import { FilterByDescriptionPipe } from './filter-by-description.pipe';
-
-import { PhotoModule } from '../photo/photo.module';
-import { CardModule } from 'src/app/shared/components/card/card.module';
-import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,6 @@ import { SearchComponent } from './search/search.component';
 
     FilterByDescriptionPipe,
   ],
-  imports: [CommonModule, PhotoModule, CardModule],
+  imports: [CommonModule, PhotoModule, CardModule, DarkenOnHoverModule],
 })
 export class PhotoListModule {}
